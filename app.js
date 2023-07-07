d5b2123b836ddf4b925e4053de4f9ee3aa8f556e
amankatiyar626 <amankatiyar626@gmail.com>
2023-07-07 15:32:14 +0530

    git push

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const app = express();
+
 const user = require('./routes/user');
+
+
+const app = express();
 
 var cors = require('cors');
 app.use(cors());
-
-
-
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
